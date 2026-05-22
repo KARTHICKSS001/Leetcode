14581ae09a4d8564a4a4beac9c134e4ea555ce1f
KARTHICKSS001 <131766698+KARTHICKSS001@users.noreply.github.com>
2026-05-22 23:45:14 +0530

    Update reverse_linked_list2.py

--- linked list/reverse_linked_list2.py
+++ linked list/reverse_linked_list2.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
