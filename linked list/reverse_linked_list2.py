0e031355598844098f11419ce3fdb82cedf19c9c
KARTHICKSS001 <131766698+KARTHICKSS001@users.noreply.github.com>
2026-05-22 23:46:46 +0530

    reverse_linked_list2.py

--- linked list/reverse_linked_list2.py
+++ linked list/reverse_linked_list2.py
@@ -7,7 +7,6 @@ class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
         prev = dummy
-
         for _ in range(left-1):
             prev = prev.next
         
